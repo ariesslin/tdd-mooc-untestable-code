ce2c1261417b5c7401d445711b16a78c43f05a55
Ariess Lin <ariess.lin@gmail.com>
2023-02-05 22:37:23 +0100

    EXEC2 | Pass die values as parameters to diceHandValue and first test

--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -2,8 +2,8 @@ import { expect } from "chai";
 import { diceHandValue } from "../src/untestable2.mjs";
 
 describe("Untestable 2: a dice game", () => {
-  it("todo", () => {
-    // TODO: write proper tests
-    expect(diceHandValue()).to.be.a("number");
+  it("die1 equals die2, value is 1", () => {
+    expect(diceHandValue(1, 1)).to.be.a("number");
+    expect(diceHandValue(1, 1)).to.equal(101);
   });
 });
